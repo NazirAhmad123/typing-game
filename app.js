d2b00e694a711594965f77247fcf2b147efd8177
Nazir <nazirahmadsaidzada@gmail.com>
2025-08-14 19:47:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -155,7 +155,7 @@ async function getNewPassage() {
     const payload = {
       contents: chatHistory
     };
-    const apiKey = "";
+    const apiKey = "AIzaSyCZUKYIA--rt75WzoFE73TrcQqbpZ7amlU";
     const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=${apiKey}`;
 
     const response = await retryFetch(apiUrl, {
